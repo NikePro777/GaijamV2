4d687351a8fb109f882e68a30217062e8d6e7791
Evgeniy Datskij <evgenij.datskij@mail.ru>
2023-11-01 09:17:30 +0500

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+    <section className="max-container h-screen padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
       <div className="hero-map" />
 
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
@@ -13,24 +13,10 @@ export default function Home() {
           height={50}
           className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
         /> */}
-        <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
+        <h1 className="bold-52 lg:bold-88">Многопрофильная организация</h1>
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur sapiente, totam velit
-          iusto inventore adipisci. Aliquam repudiandae eveniet libero sed voluptatibus, dignissimos
-          ex, nesciunt voluptates doloremque tenetur fugiat delectus ad laboriosam quia quasi nisi
-          vero ipsam? Repudiandae, nobis rerum. Expedita, blanditiis ratione eos est, et placeat
-          modi quis nam amet laudantium harum ducimus architecto. Libero esse impedit quasi culpa
-          quam saepe. Animi hic, a quisquam nulla odit dignissimos facilis accusamus ipsum illo
-          error quia consequuntur alias. Quaerat dicta sit, voluptatem reiciendis aliquam nobis
-          quibusdam corrupti optio, labore itaque ea quos animi laudantium expedita eos soluta alias
-          dignissimos accusamus praesentium illum ipsa repellat tempore consequuntur? Tenetur,
-          consectetur laboriosam provident quasi quas minima sapiente hic! Ab hic ullam ea illo
-          excepturi soluta sunt ratione architecto veritatis id recusandae, nemo, deserunt officiis
-          molestias ad harum impedit voluptatum! Fuga, praesentium? Fugit, quisquam perspiciatis
-          voluptatum dolorem omnis velit optio aliquam porro impedit nulla! Ipsa reiciendis ducimus
-          nihil repellat labore nesciunt perspiciatis beatae eius eveniet sapiente, nemo quod error
-          deserunt nam nulla! Deserunt ut quod qui quas natus voluptas expedita quae magni
-          voluptates incidunt mag
+          Мы - единственная в Екатеринбурге компания, способная доставить машину комфорт класса по
+          доступной цене.
         </p>
 
         <div className="my-11 flex flex-wrap gap-5">
@@ -63,7 +49,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative flex flex-1 items-start">
+      <div className="relative flex flex-1 items-center">
         <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
           <div className="flex flex-col">
             <div className="flexBetween">
