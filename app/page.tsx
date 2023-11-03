f04d94b5c7f7b144afaa6720b9a8042c1b8d43a6
Evgeniy Datskij <evgenij.datskij@mail.ru>
2023-11-03 13:44:19 +0500

    finish adaptov 1st section main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,41 @@
 import Image from 'next/image';
+import mountains from '../public/avto-logo1.png';
+
+function Background() {
+  return (
+    <Image
+      alt="Mountains"
+      src={mountains}
+      placeholder="blur"
+      quality={100}
+      fill
+      sizes="100vw"
+      style={{
+        objectFit: 'cover',
+      }}
+    />
+  );
+}
 
 export default function Home() {
   return (
     <>
       {/* gap-20 py-10 md:gap-28 */}
-      <section className="max-container padding-container flex flex-col  xl:flex-row min-h-[850px] max-h-[850px]">
-        <div className="hero-map min-h-[450px] max-h-[450px] md:min-h-[800px]" />
+      <section className="max-container padding-container flex flex-col xl:flex-row">
+        <div className="hero-map min-h-[450px] max-h-[450px] md:min-h-[740px]" />
 
         <div className="relative z-20 flex flex-1 flex-col ">
           {/* 2xl:pb-9 */}
           <div className="flexCenter w-full flex-col ">
             {/* lg:bold-88 */}
-            <h1 className="cooltext bold-52 py-8 xs:py-12 md:bold-64">GaiJam</h1>
+            <h1 className="cooltext  py-8 xs:py-12 ">GaiJam</h1>
             {/* bold-52xl:bold-40 */}
-            <p className="text-[16px] pt-10 font-[600] xs:bold-16 sm:bold-20 text-black-100 md:bold-26 lg:bold-40">
+            <p className="bold-20 sm:bold-26 text-black-100 md:bold-32 ">
               Мы - единственная в Екатеринбурге многопрофильная компания, способная доставить машину
               комфорт класса по доступной цене.
             </p>
 
-            <div className="my-11 flex flex-wrap gap-5 xxs:pt-8 md:pt-24 lg:pt-8">
+            <div className="my-11 flex flex-wrap gap-2 xxs:pt-6 md:pt-12 lg:pt-8">
               <div className="flex items-center gap-2">
                 {Array(5)
                   .fill(1)
@@ -36,19 +53,12 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="max-container h-screen padding-container">
+      <section className="pt-3 max-container h-screen padding-container">
         <div className="relative z-20 flex flex-1 flex-col">
-          {/* <Image
-          src="/camp.svg"
-          alt="camp"
-          width={50}
-          height={50}
-          className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
-        /> */}
-          <h1 className="cooltext bold-52 lg:bold-88">GaiJam</h1>
+          {/* <div>{Background()}</div> */}
+          <h1 className="cooltext2 pt-10">Автосервис</h1>
           <p className="bold-52 mt-3 text-black-100 ">
-            Мы - единственная в Екатеринбурге многопрофильная компания, способная доставить машину
-            комфорт класса по доступной цене.
+            Один из лучших сервисов Екатеринбурга в соотношении цена/качество
           </p>
 
           <div className="my-11 flex flex-wrap gap-5">
