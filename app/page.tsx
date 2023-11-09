304cf3558fd5f3900ad0794d198edeb2987537ae
Evgeniy Datskij <evgenij.datskij@mail.ru>
2023-11-09 18:29:54 +0500

    nedd center button

--- app/page.tsx
+++ app/page.tsx
@@ -63,24 +63,12 @@ export default function Home() {
       </section>
 
       <section className="bg-gray-100 w-screen h-[800px] relative overflow-hidden">
-        <div className="absolute inset-0 flex items-center justify-center">
+        <div className="absolute inset-0">
           <img src="./mers.png" alt="Ваше изображение" className="object-cover w-full h-full" />
         </div>
-        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white">
-          <h2 className="cooltext2 my-8">Современное авто без вложений!</h2>
-          {/* <p className="text-3xl my-4">
-            Запись по телефону
-            <a href="tel:+79581365095" className="text-blue-500 hover:underline ml-4">
-              +7 (958) 136-5095
-            </a>
-          </p>
-          <ul className="text-left p-10">
-            <li className="text-2xl text-gray-20 mb-2">Работаем для Вас</li>
-            <li className="text-2xl text-gray-30 mb-2">Ежедневно с 09:00 до 21:00</li>
-            <li className="text-2xl text-gray-20 mb-2">По адресу</li>
-            <li className="text-2xl text-gray-30 mb-2">ул. Пехотинцев д.4</li>
-          </ul> */}
-          <div className="flex justify-center items-center">
+        <div className="absolute inset-0  items-center justify-center bg-black bg-opacity-50 text-white">
+          <h2 className="cooltext2 my-8 mx-auto text-center">Современное авто без вложений!</h2>
+          <div className="mx-auto text-center">
             <Button
               type="submit"
               title="подробнее"
