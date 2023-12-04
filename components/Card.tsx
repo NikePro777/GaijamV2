33a1cd9be75acb5a9ffca136a0ef9b0c17ddb949
Evgeniy Datskij <evgenij.datskij@mail.ru>
2023-12-04 23:01:20 +0500

    add style

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,13 +3,15 @@ import Image from 'next/image';
 type CardProps = {
   link: string;
   title: string;
+  text: string;
 };
 
-const Card = ({ link, title }: CardProps) => {
+const Card = ({ link, title, text }: CardProps) => {
   return (
     <div className="w-280 text-center mb-65 transition duration-200 ease-in-out">
       <Image className="w-260" width={260} height={100} src={link} alt="Ваше изображение" />
       <h4 className="text-20 font-bold tracking-wider mb-20">{title}</h4>
+      <p>{text}</p>
     </div>
   );
 };
