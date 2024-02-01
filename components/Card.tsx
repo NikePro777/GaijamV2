658a397e8ca92954298c90bae4c540b64bc18185
Evgeniy Datskij <evgenij.datskij@mail.ru>
2024-02-01 18:36:13 +0500

    fix service

--- components/Card.tsx
+++ components/Card.tsx
@@ -11,7 +11,7 @@ type CardProps = {
 const Card = ({ link, title, text, height = 225, weight = 260 }: CardProps) => {
   const CN = `w-[${weight}px] blockCenter h-[${height}px]`;
   return (
-    <div className="md:w-[300px] w-[500px] mb-7 text-center blockCenter transition duration-200 ease-in-out">
+    <div className="md:w-[300px] mb-7 text-center blockCenter transition duration-200 ease-in-out ">
       <Image className={CN} width={weight} height={height} src={link} alt="Ваше изображение" />
 
       <h4 className="bold-x tracking-wider my-3">{title}</h4>
